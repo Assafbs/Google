439cad098c72741ff1e79406bab0178aa27c1d35
Assaf Ben Shimon <asafbs@gmail.com>
2017-04-14 16:46:08 +0300

    start button links to login page
    + added some localization

--- Mezu/app/src/main/java/money/mezu/mezu/MainActivity.java
+++ Mezu/app/src/main/java/money/mezu/mezu/MainActivity.java
@@ -1,8 +1,11 @@
 package money.mezu.mezu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = (Button)findViewById(R.id.start_btn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: check if user is logged in already
+                Intent budgetsIntent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(budgetsIntent);
+            }
+        });
     }
 }
