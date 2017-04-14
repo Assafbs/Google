741ba787ba341968471765fae43a5173b98d784a
Assaf Ben Shimon <asafbs@gmail.com>
2017-04-14 14:15:19 +0300

    Added Basic Starting Layout
    + Changed "AppCompatActivity" to "Activity" to eliminate default title in the top

--- Mezu/app/src/main/java/money/mezu/mezu/MainActivity.java
+++ Mezu/app/src/main/java/money/mezu/mezu/MainActivity.java
@@ -1,9 +1,10 @@
 package money.mezu.mezu;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
