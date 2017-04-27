16dcd286c486ebc71bd281a509c7c44666e8fe63
lior-zilberstein <zilberstein92@gmail.com>
2017-04-28 00:06:28 +0300

    Yet another nav drawer preperation
    getting closer!

--- Mezu/app/src/main/java/money/mezu/mezu/MainNavDrawerActivity.java
+++ Mezu/app/src/main/java/money/mezu/mezu/MainNavDrawerActivity.java
@@ -1,7 +1,6 @@
 package money.mezu.mezu;
 
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,19 +9,65 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainNavDrawerActivity extends AppCompatActivity {
 
+    String TITLES[] = {"Home","Events","Mail","Shop","Travel"};
+    int ICONS[] = {R.mipmap.budget_icon,
+            R.mipmap.logout_icon,
+            R.mipmap.add_budget_icon,
+            R.mipmap.menu_icon,
+            R.mipmap.nav_drawer_icon};
+
+    String NAME = "Akash Bangad";
+    String EMAIL = "akash.bangad@android4devs.com";
+    int PROFILE = R.drawable.rich_man_small;
+
+    private Toolbar toolbar;
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    DrawerLayout Drawer;
+    ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.main_nav_drawer_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+
+        /* Assinging the toolbar object ot the view and setting the the Action bar to our toolbar*/
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new NavDrawerAdapter(TITLES,ICONS,NAME,EMAIL,PROFILE);
+
+        mRecyclerView.setAdapter(mAdapter);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+        };
+
+        Drawer.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
     }
 
     @Override
