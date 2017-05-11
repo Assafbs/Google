90c6aa811e87f6b7d76972773613abd24a575b3e
Assaf Ben Shimon <asafbs@gmail.com>
2017-05-12 00:02:57 +0300

    added initial values and callback stubs in settings
    + small modifications

--- Mezu/app/src/main/java/money/mezu/mezu/SettingsActivity.java
+++ Mezu/app/src/main/java/money/mezu/mezu/SettingsActivity.java
@@ -3,8 +3,14 @@ package money.mezu.mezu;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.Locale;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -23,6 +29,55 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.prefs);
+            SessionManager sessionManager = new SessionManager(getActivity());
+
+            EditTextPreference displayNamePref = (EditTextPreference)findPreference("display_name");
+            displayNamePref.setText(sessionManager.getUserName());
+            displayNamePref.setSummary(sessionManager.getUserName());
+            displayNamePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    // TODO: update name in DB
+                    return false;
+                }
+            });
+
+            ListPreference languagePref = (ListPreference)findPreference("language");
+            languagePref.setSummary(getCurrentLanguage()); // temp
+            // TODO: take current value from shared preferences
+            languagePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    // TODO: change language and save in shared preference
+                    preference.setSummary(getLanguageFromValue(o.toString()));
+                    return true;
+                }
+            });
+
+            SwitchPreference enableNotificationsPref = (SwitchPreference)findPreference("enable_notifications");
+            // TODO: take current value from shared preferences
+            enableNotificationsPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    // TODO: save decision in shared preference
+                    return true;
+                }
+            });
+        }
+
+        private String getCurrentLanguage(){
+            return getLanguageFromValue(Locale.getDefault().getISO3Language());
+        }
+
+        private String getLanguageFromValue(String lang){
+            if (lang.equals("heb")){
+                return getString(R.string.hebrew);
+            }
+            else {
+                return getString(R.string.english);
+            }
         }
     }
+
+
 }
