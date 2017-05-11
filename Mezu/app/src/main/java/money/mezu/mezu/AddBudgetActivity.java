e909d620b04a9037c1a439ba154932e0b0bf5364
ordav <ordav7@gmail.com>
2017-05-11 17:29:28 +0300

    Added contacts mails autocomplete when trying to share budget

--- Mezu/app/src/main/java/money/mezu/mezu/AddBudgetActivity.java
+++ Mezu/app/src/main/java/money/mezu/mezu/AddBudgetActivity.java
@@ -1,23 +1,37 @@
 package money.mezu.mezu;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.Manifest;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.attr.id;
+import android.os.Build;
+
 public class AddBudgetActivity extends BaseNavDrawerActivity {
 
     List<String> partnersEmails;
     TextView partnersList;
+    ArrayList<String> contactEmails;
+
+    // Request code for READ_CONTACTS. It can be any number > 0.
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,25 @@ public class AddBudgetActivity extends BaseNavDrawerActivity {
 
         partnersEmails = new ArrayList<String>();
         partnersList = (TextView)findViewById(R.id.partners_list);
+
+        contactEmails = new ArrayList<>();
+        final AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.partner_email);
+        final AddBudgetActivity activity = this;
+        final boolean[] clicked = new boolean[1];
+        clicked[0] = false;
+        textView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!clicked[0] && hasFocus) {
+                    tryInitializingContactEmails();
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
+                            android.R.layout.simple_dropdown_item_1line, contactEmails);
+                    textView.setAdapter(adapter);
+                    clicked[0] = true;
+                }
+            }
+        });
 
         Button addBudgetBtn = (Button) findViewById(R.id.add_budget);
         addBudgetBtn.setOnClickListener(new View.OnClickListener() {
@@ -83,5 +116,48 @@ public class AddBudgetActivity extends BaseNavDrawerActivity {
         //TODO: validate email; if function is implemented somewhere else, use same.
         //TODO: maybe check if it's in the system, and if not send and invitation to Mezu?
         return true;
+    }
+
+    public void tryInitializingContactEmails() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+        }
+        else {
+            initializeContactEmails();
+        }
+    }
+
+    public void initializeContactEmails() {
+        ContentResolver cr = getContentResolver();
+        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        while (cursor.moveToNext()) {
+            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor emailCur = cr.query(
+                    ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                    null,
+                    ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                    new String[]{id}, null);
+            while (emailCur.moveToNext()) {
+                String email = emailCur.getString(emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                //Log.d("", String.format("AddBudgetActivity: foo2, email is %s", email));
+                contactEmails.add(email);
+
+            }
+            emailCur.close();
+        }
+        cursor.close();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                initializeContactEmails();
+            } else {
+                //Toast.makeText(this, "Until you grant the permission, there will be no autocomplete for emails", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
