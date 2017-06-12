e895df90d9dee66e98256a47c38d3c501b13da16
randomUserNameThatIsRandom <JB@JBs-MacBook-Air.local>
2017-06-12 15:25:05 +0300

    adding notifications for joining budget and new expence

---
+++ Mezu/app/src/main/java/money/mezu/mezu/MyFirebaseMessagingService.java
@@ -0,0 +1,23 @@
+package money.mezu.mezu;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by JB on 6/12/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService
+{
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d("", "MyFirebaseMessagingService::onMessageReceived: From: " + remoteMessage.getFrom());
+        Log.d("", "MyFirebaseMessagingService::onMessageReceived: Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+
+}
--- functions/index.js
+++ functions/index.js
@@ -6,20 +6,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-
-/**
- Triggers when a user's budget assosiation has changed.
- */
-exports.sendAddedToBudgetNotification = functions.database.ref('/users/{uid}/budgets/{bid}').onWrite(event => {
-	const uid = event.params.uid;
-	const bid = event.params.bid;
-	// If un-follow we exit the function.
-	if (!event.data.val()) 
-	{
-		return console.log('User ', uid, ' has left budget ', bid);
-	}
-	console.log('User ', uid, ' was added to budget ', bid);
-
+function sendNotification(uid, title, body)
+{
 	// Get notification token for user.
 	const deviceNotificationTokenPromise = admin.database().ref('/users/'+uid + '/notificationToken').once('value').then(function(snapshot) {
 		const deviceNotificationToken = snapshot.val(); 
@@ -30,8 +18,8 @@ exports.sendAddedToBudgetNotification = functions.database.ref('/users/{uid}/bud
 		{
 			notification: 
 			{
-				title: 'You were added to a budget!',
-				//body: `${follower.displayName} is now following you.`,
+				title: title,
+				body: body,
 				//icon: follower.photoURL
 			}
 		};
@@ -39,8 +27,53 @@ exports.sendAddedToBudgetNotification = functions.database.ref('/users/{uid}/bud
 		// Send notifications to all tokens.
 		return admin.messaging().sendToDevice(deviceNotificationToken, payload);
 	});	
-});	
+}
+/**
+ Triggers when a user's budget assosiation has changed.
+ */
+ //******************************************************************************************************************************************************
+exports.sendAddedToBudgetNotification = functions.database.ref('/users/{uid}/budgets/{bid}').onWrite(event => {
+	const uid = event.params.uid;
+	const bid = event.params.bid;
+	// If un-follow we exit the function.
+	if (!event.data.val()) 
+	{
+		return console.log('User ', uid, ' has left budget ', bid);
+	}
+	console.log('User ', uid, ' was added to budget ', bid);
+	sendNotification(uid, "You were added to a budget!", "");
+	
 
+	
+});	
+//******************************************************************************************************************************************************
+exports.sendExpenseNotification = functions.database.ref('/budgets/{bid}/budget/mExpenses/{eid}').onWrite(event => {
+	const eid = event.params.eid;
+	const bid = event.params.bid;
+	// If un-follow we exit the function.
+	if (!event.data.val()) 
+	{
+		return console.log('Expense ', eid ,' deleted from budget ', bid);
+	}
+	console.log('Expense ', eid, ' was added to budget ', bid);
+
+	const uidsPromise = admin.database().ref("/budgets/" + bid).once('value').then(function(snapshot)
+	{
+		console.log('Sending notification on budget: ' + bid);;
+		const users = snapshot.child("users").val();
+		const budgetName = snapshot.child("budget").child("mName").val();
+		const userName = snapshot.child("budget").child("mExpenses").child(eid).child("mUserName").val();
+		const expenseAmount = snapshot.child("budget").child("mExpenses").child(eid).child("mAmount").val();
+		console.log('Expense added by ' + userName);
+		for (var uid in users) 
+		{
+			console.log('sending notification to ' + uid);
+			sendNotification(uid, "New expense was added to budget: " + budgetName, "By: " + userName + "\nFor: " + expenseAmount);
+		}
+		
+	});
+
+});	
 
 
 
