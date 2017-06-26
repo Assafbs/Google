94108563c25d086ea8ad04167bdd2fd347f90062
lederdavid <lederkremer@gmail.com>
2017-06-26 19:46:15 +0300

    Graphs refresh bug fixed forever!

--- Mezu/app/src/main/java/money/mezu/mezu/GraphsTabFragment.java
+++ Mezu/app/src/main/java/money/mezu/mezu/GraphsTabFragment.java
@@ -15,21 +15,16 @@ public class GraphsTabFragment extends Fragment implements ExpenseUpdatedListene
 
     private View mView = null;
     private GraphAdapter mGraphAdapter = null;
-    private Resources resources = StaticContext.mContext.getResources();
-
+    private ListView mListView;
     private BudgetViewActivity mActivity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.tab_graphs, container, false);
         mActivity = (BudgetViewActivity) getActivity();
-        EventDispatcher.getInstance().registerExpenseUpdateListener(this);
+        mListView = (ListView) mView.findViewById(R.id.graphs_list);
+        refreshGraphAdapter();
         EventDispatcher.getInstance().registerBudgetUpdateListener(this);
-        // Create the adapter to convert the array to views
-        mGraphAdapter = new GraphAdapter(getActivity(), getArrayOfGraphs());
-        // Attach the adapter to a ListView
-        ListView listView = (ListView) mView.findViewById(R.id.graphs_list);
-        listView.setAdapter(mGraphAdapter);
         setNoExpensesIndication();
         return mView;
     }
@@ -37,36 +32,40 @@ public class GraphsTabFragment extends Fragment implements ExpenseUpdatedListene
     public ArrayList<GraphInterface> getArrayOfGraphs() {
         Budget budget = mActivity.mCurrentBudget;
         ArrayList<GraphInterface> graphArray = new ArrayList<>();
-        PieChartCategories pieChartCategories = new PieChartCategories(mActivity.mCurrentBudget);
-        LineChartMonths lineChartMonths = new LineChartMonths(mActivity.mCurrentBudget);
-        BarChartUsers barChartUsers = new BarChartUsers(mActivity.mCurrentBudget);
+        PieChartCategories pieChartCategories = new PieChartCategories(budget);
+        LineChartMonths lineChartMonths = new LineChartMonths(budget);
+        BarChartUsers barChartUsers = new BarChartUsers(budget);
 
         //ADD GRAPHS HERE! ONE PER EACH GRAPH CREATED
-        if (mActivity.mCurrentBudget.getTotalExpenses() > 0) {
+        if (budget.getTotalExpenses() > 0) {
             graphArray.add(pieChartCategories);
         }
-        if (mActivity.mCurrentBudget.getTotalExpenses() > 0 || mActivity.mCurrentBudget.getTotalIncomes() > 0) {
+        if (budget.getTotalExpenses() > 0 || budget.getTotalIncomes() > 0) {
             graphArray.add(lineChartMonths);
         }
-        if (mActivity.mCurrentBudget.getTotalExpenses() > 0) {
+        if (budget.getTotalExpenses() > 0 && budget.getArrayOfUserNamesExpensesOnly().size() > 1) {
             graphArray.add(barChartUsers);
         }
-
-//            graphArray.add(pieChartCategories);
-//            graphArray.add(lineChartMonths);
-//            graphArray.add(barChartUsers);
-
         return graphArray;
     }
 
     @Override
     public void budgetUpdatedCallback(Budget newBudget) {
-        mGraphAdapter.notifyDataSetChanged();
+        refreshGraphAdapter();
+        setNoExpensesIndication();
     }
 
     @Override
     public void expenseUpdatedCallback() {
-        mGraphAdapter.notifyDataSetChanged();
+        refreshGraphAdapter();
+        setNoExpensesIndication();
+    }
+
+    public void refreshGraphAdapter() {
+        // Create the adapter to convert the array to views
+        mGraphAdapter = new GraphAdapter(mActivity, getArrayOfGraphs());
+        // Attach the adapter to a ListView
+        mListView.setAdapter(mGraphAdapter);
     }
 
     private void setNoExpensesIndication() {
@@ -74,7 +73,7 @@ public class GraphsTabFragment extends Fragment implements ExpenseUpdatedListene
             mView.findViewById(R.id.graphs_list).setVisibility(View.GONE);
             mView.findViewById(R.id.explaining_text2).setVisibility(View.VISIBLE);
             mView.findViewById(R.id.crying_logo).setVisibility(View.VISIBLE);
-        }else{
+        } else {
             mView.findViewById(R.id.graphs_list).setVisibility(View.VISIBLE);
             mView.findViewById(R.id.explaining_text2).setVisibility(View.GONE);
             mView.findViewById(R.id.crying_logo).setVisibility(View.GONE);
