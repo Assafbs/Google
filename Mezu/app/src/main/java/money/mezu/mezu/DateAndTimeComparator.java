358849f2565f0c722ffc6d97996775ed430ee488
lederdavid <lederkremer@gmail.com>
2017-08-15 17:50:43 +0300

    Actions are now sorted by date and time, new on top

--- Mezu/app/src/main/java/money/mezu/mezu/DateAndTimeComparator.java
+++ Mezu/app/src/main/java/money/mezu/mezu/DateAndTimeComparator.java
@@ -10,6 +10,6 @@ public class DateAndTimeComparator implements Comparator<Expense> {
 
     @Override
     public int compare(Expense e1, Expense e2) {
-        return e1.getTime().compareTo(e2.getTime());
+        return -1 * e1.getTime().compareTo(e2.getTime());
     }
 }
